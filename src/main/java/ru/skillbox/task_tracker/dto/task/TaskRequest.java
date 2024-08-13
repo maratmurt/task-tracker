979887996254fc1cf50497216712945069fda944
Marat Murtuzaliev <marat.murtuzaliev@gmail.com>
2024-08-13 16:11:57 +0300

    refactor: change task dto to record

--- src/main/java/ru/skillbox/task_tracker/dto/task/TaskRequest.java
+++ src/main/java/ru/skillbox/task_tracker/dto/task/TaskRequest.java
@@ -1,71 +1,19 @@
 package ru.skillbox.task_tracker.dto.task;
 
-import lombok.EqualsAndHashCode;
-import lombok.ToString;
-
 import java.util.List;
 
-@ToString
-@EqualsAndHashCode
-public class TaskRequest {
+public record TaskRequest(
 
-    private String name;
+    String name,
 
-    private String description;
+    String description,
 
-    private String status;
+    String status,
 
-    private String authorId;
+    String authorId,
 
-    private String assigneeId;
+    String assigneeId,
 
-    private List<String> observerIds;
+    List<String> observerIds
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public String getAuthorId() {
-        return authorId;
-    }
-
-    public void setAuthorId(String authorId) {
-        this.authorId = authorId;
-    }
-
-    public String getAssigneeId() {
-        return assigneeId;
-    }
-
-    public void setAssigneeId(String assigneeId) {
-        this.assigneeId = assigneeId;
-    }
-
-    public List<String> getObserverIds() {
-        return observerIds;
-    }
-
-    public void setObserverIds(List<String> observerIds) {
-        this.observerIds = observerIds;
-    }
-}
+){}
--- src/main/java/ru/skillbox/task_tracker/dto/task/TaskResponse.java
+++ src/main/java/ru/skillbox/task_tracker/dto/task/TaskResponse.java
@@ -1,102 +1,27 @@
 package ru.skillbox.task_tracker.dto.task;
 
-import lombok.EqualsAndHashCode;
-import lombok.ToString;
 import ru.skillbox.task_tracker.dto.user.UserResponse;
 
 import java.util.List;
 
-@ToString
-@EqualsAndHashCode
-public class TaskResponse {
+public record TaskResponse(
 
-    private String id;
+     String id,
 
-    private String name;
+     String name,
 
-    private String description;
+     String description,
 
-    private Long createdAt;
+     Long createdAt,
 
-    private Long updatedAt;
+     Long updatedAt,
 
-    private String status;
+     String status,
 
-    private UserResponse author;
+     UserResponse author,
 
-    private UserResponse assignee;
+     UserResponse assignee,
 
-    private List<UserResponse> observers;
+     List<UserResponse> observers
 
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Long getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(Long createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public Long getUpdatedAt() {
-        return updatedAt;
-    }
-
-    public void setUpdatedAt(Long updatedAt) {
-        this.updatedAt = updatedAt;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public UserResponse getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(UserResponse author) {
-        this.author = author;
-    }
-
-    public UserResponse getAssignee() {
-        return assignee;
-    }
-
-    public void setAssignee(UserResponse assignee) {
-        this.assignee = assignee;
-    }
-
-    public List<UserResponse> getObservers() {
-        return observers;
-    }
-
-    public void setObservers(List<UserResponse> observers) {
-        this.observers = observers;
-    }
-}
+){}
